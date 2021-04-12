5efc5f2e011837a108e39597a78576e2f2dfc470
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-04-12 22:33:23 +0200

    done

---
+++ Etudiant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Partie_11_Serialization_Json
+{
+    [Serializable]
+    internal class Etudiant
+    {
+        public int numins { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public override string ToString()
+        {
+            return $"numero d'inscription : {numins} - nom : {nom} - prenom : {prenom}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Partie_11_Serialization_Json
+{
+    class Program
+    {
+        const string path = @"C:\Users\ISSAM\Desktop\Web Cote Serveur\cours\exircice de cours\Partie 11 Serialization Json\etudiant.json";
+        static void Main(string[] args)
+        {
+            Deserialize();
+            Console.ReadKey();
+        }
+        static private void Serialize()
+        {
+            String etudiantJSON = JsonConvert.SerializeObject(new Etudiant()
+            {
+                numins = 1,
+                nom = "boutissane",
+                prenom = "issam"
+            });
+            File.WriteAllText(path, etudiantJSON);
+            Console.WriteLine(etudiantJSON);
+            Console.WriteLine("Serialization is done");
+        }
+        static private void Deserialize()
+        {
+            Etudiant etudiant = JsonConvert.DeserializeObject<Etudiant>(File.ReadAllText(path));
+            Console.WriteLine(etudiant);
+        }
+    }
+}
